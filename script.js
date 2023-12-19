dfed5bfd8e123bbd02bf7c662e8130d957167c8c
NorielAchero <norielrachero@gmail.com>
2023-12-19 23:23:24 +0800

    Added Calculation and KeyEvents

--- script.js
+++ script.js
@@ -17,3 +17,57 @@ function screenProcessing(value){
 
 }
 
+document.addEventListener('keydown', function(event) {
+
+    if (event.keyCode === 48 || event.keyCode === 96) {
+      document.getElementById('0').click();
+    }
+    else if(event.keyCode === 49 || event.keyCode === 97) {
+        document.getElementById('1').click();
+    }
+    else if(event.keyCode === 50 || event.keyCode === 98) {
+        document.getElementById('2').click();
+    }
+    else if(event.keyCode === 51 || event.keyCode === 99) {
+        document.getElementById('3').click();
+    }
+    else if(event.keyCode === 52 || event.keyCode === 100) {
+        document.getElementById('4').click();
+    }
+    else if(event.keyCode === 53 || event.keyCode === 101) {
+        document.getElementById('5').click();
+    }
+    else if(event.keyCode === 54 || event.keyCode === 102) {
+        document.getElementById('6').click();
+    }
+    else if(event.keyCode === 55 || event.keyCode === 103) {
+        document.getElementById('7').click();
+    }
+    else if(event.keyCode === 56 || event.keyCode === 104) {
+        document.getElementById('8').click();
+    }
+    else if(event.keyCode === 57 || event.keyCode === 105) {
+        document.getElementById('9').click();
+    }
+    else if(event.keyCode === 110) {
+        document.getElementById('.').click();
+    }
+    else if(event.keyCode === 107) {
+        document.getElementById('+').click();
+    }
+    else if(event.keyCode === 109) {
+        document.getElementById('-').click();
+    }
+    else if(event.keyCode === 106) {
+        document.getElementById('*').click();
+    }
+    else if(event.keyCode === 111) {
+        document.getElementById('/').click();
+    }
+    else if(event.keyCode === 13) {
+        document.getElementById('enter').click();
+    }
+    else if(event.keyCode === 8) {
+        document.getElementById('clear').click();
+    }
+  })
